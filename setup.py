8fe907f41b183460af42472933491f3caba2f993
Parimal_Shinde <parimalshinde3@gmail.com>
2024-10-05 16:23:40 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+HYPHEN_E='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return requirements
+    '''
+    
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPHEN_E in requirements:
+            requirements.remove(HYPHEN_E)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Parimal",
+    author_email="Parimalshinde3@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
